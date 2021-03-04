d650e20a2548c61364a5103065ee86e7e3787329
Clotilde LEVESQUE <clotilde.levesque@epita.fr>
2021-03-04 17:58:08 +0100

    test: one case added

--- tests/unit_tests/chip8/chip8_unit.c
+++ tests/unit_tests/chip8/chip8_unit.c
@@ -20,3 +20,12 @@ Test(instructions_set, zero_case)
     free_chip8(chip);
 }
 
+Test(instructions_set, one_case)
+{
+    chip8 *chip = init_chip8();
+    uint16_t opcode = 0x1666;
+    uint16_t address = 0x0666;
+    one_case(chip, opcode);
+    cr_expect_eq(chip->PC, address);
+    free_chip8(chip);
+}
