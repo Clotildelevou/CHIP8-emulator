a52b8bb700dc8ada747b661ef7a39da390ad9c84
Clotilde LEVESQUE <clotilde.levesque@epita.fr>
2021-03-18 01:33:26 +0100

    test: seven case

--- tests/unit_tests/chip8/chip8_unit.c
+++ tests/unit_tests/chip8/chip8_unit.c
@@ -126,3 +126,16 @@ Test(instructions_set, six_case)
     cr_expect_eq(chip->V[5], res);
     free_chip8(chip);
 }
+
+Test(instructions_set, seven_case)
+{
+    chip8 *chip = init_chip8();
+    uint16_t opcode = 0x7501;
+    chip->V[5] = 0x0001;
+    uint16_t res = 0x0002;
+    uint16_t pc = chip->PC + 2;
+    seven_case(chip, opcode);
+    cr_expect_eq(chip->PC, pc);
+    cr_expect_eq(chip->V[5], res);
+    free_chip8(chip);
+}
